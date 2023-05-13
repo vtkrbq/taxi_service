c2e323a3b5f815c4fa5f2f0a11a04e5cc9293cb3
Andrey Ivanov <1992.ivanov.andrey@gmail.com>
2023-05-13 20:39:46 +0200

    connection provider

--- taxi_service_data/src/main/java/ua/rudniev/taxi/connection/ConnectionProvider.java
+++ taxi_service_data/src/main/java/ua/rudniev/taxi/connection/ConnectionProvider.java
@@ -8,22 +8,24 @@ import java.sql.SQLException;
 
 /**
  * This class has fields and methods that provides connection to PostgreSQL Data Base
-**/
+ **/
 public class ConnectionProvider {
     private static final HikariConfig config = new HikariConfig();
     private static final HikariDataSource ds;
 
     static {
         config.setDriverClassName("org.postgresql.Driver");
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/taxi_service");
+        config.setJdbcUrl(System.getenv("TAXI_SERVICE_DB_URL"));
         config.setAutoCommit(false);
-        config.setUsername("vtkrbq");
-        config.setPassword("PolotencE123");
+        config.setUsername(System.getenv("TAXI_SERVICE_DB_LOGIN"));
+        config.setPassword(System.getenv("TAXI_SERVICE_DB_PASSWORD"));
         ds = new HikariDataSource(config);
     }
+
     public static Connection getConnection() throws SQLException {
         return ds.getConnection();
     }
 
-    private ConnectionProvider() {}
+    private ConnectionProvider() {
+    }
 }
